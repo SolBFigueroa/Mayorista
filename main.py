a45d5fafae110110200a7260c3205c6693f782c4
SolBFigueroa <figueroa.solb@gmail.com>
2026-05-28 19:15:15 -0300

    configuro main.py y creo tablas en la base de datos

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 from fastapi import FastAPI
+from app.database import engine, Base
+from app.models import usuarios, productos, ventas
+Base.metadata.create_all(bind=engine)
 app = FastAPI()
 # acá van a ir los routers después
