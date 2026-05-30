014e6c626e0955009640d8d5b567a8dde790259d
SolBFigueroa <figueroa.solb@gmail.com>
2026-05-30 11:33:58 -0300

    agrego rutas de productos

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
 from app.database import engine, Base
 from app.models import usuarios, productos, ventas
+from app.routes import productos
 Base.metadata.create_all(bind=engine)
+
 app = FastAPI()
 # acá van a ir los routers después
+app.include_router(productos.router)
+
+
+
+
