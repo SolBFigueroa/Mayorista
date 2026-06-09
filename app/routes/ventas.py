9c0080c072cbf198d71a270804ec13eaabb0f117
SolBFigueroa <figueroa.solb@gmail.com>
2026-06-09 18:18:14 -0300

    agrego autenticacion en rutas

--- app/routes/ventas.py
+++ app/routes/ventas.py
@@ -4,6 +4,7 @@ from app.database import get_db
 from app.models.ventas import Venta, DetalleVenta
 from app.schemas.ventas import VentaCrear, DetalleCrear
 from app.models.productos import Producto
+from app.seguridad import obtener_usuario_actual 
 from datetime import date
 from fastapi import HTTPException
 from fastapi import Depends, APIRouter
@@ -11,38 +12,38 @@ from fastapi import Depends, APIRouter
 router = APIRouter()
 
 @router.get("/ventas/")
-def listar_ventas( db: Session = Depends(get_db)):
+def listar_ventas(usuario: dict = Depends(obtener_usuario_actual), db: Session = Depends(get_db)):
     lista = db.query(Venta).all()
     return lista
 
 @router.post("/ventas/agregar/")
-def agregar_venta(venta : VentaCrear,  db: Session = Depends(get_db)):
+def agregar_venta(venta : VentaCrear,usuario: dict = Depends(obtener_usuario_actual), db: Session = Depends(get_db)):
     nueva_venta = Venta(user_id = venta.user_id, tipo_entrega= venta.tipo_entrega)
     db.add(nueva_venta)
     db.commit()
     return nueva_venta
 
 @router.get("/ventas/detalles/{id_venta}")
-def listar_detalles_venta(id_venta : int, db: Session = Depends(get_db)):
+def listar_detalles_venta(id_venta : int,usuario: dict = Depends(obtener_usuario_actual), db: Session = Depends(get_db)):
     venta_db = db.query(Venta).filter(Venta.id == id_venta).first()
     if venta_db is None:
         raise HTTPException(status_code=404, detail="Usuario no encontrado")
     return venta_db.detalles
 
 @router.get("/ventas/buscar/{fecha_inicio}/{fecha_fin}")
-def buscar_por_fechas_ventas(fecha_inicio: date, fecha_fin: date, db: Session = Depends(get_db)):
+def buscar_por_fechas_ventas(fecha_inicio: date, fecha_fin: date,usuario: dict = Depends(obtener_usuario_actual), db: Session = Depends(get_db)):
     ventas_db = db.query(Venta).filter(Venta.fecha >= fecha_inicio, Venta.fecha <= fecha_fin).all()
     return ventas_db
 
 @router.get("/ventas/nombre/{name}/tickets/")
-def buscar_ventas_por_producto(name: str, db: Session = Depends(get_db)):
+def buscar_ventas_por_producto(name: str,usuario: dict = Depends(obtener_usuario_actual), db: Session = Depends(get_db)):
     producto_db = db.query(Producto).filter(Producto.nombre == name).first()
     if not producto_db:
         raise HTTPException(status_code=404, detail="Producto no encontrado")
     return producto_db.detalles_venta
 
 @router.post("/ventas/nuevo/detalle/producto/{id_venta}")
-def agregar_detalle(id_venta: int, nuevo_detalle : DetalleCrear, db: Session = Depends(get_db)):
+def agregar_detalle(id_venta: int, nuevo_detalle : DetalleCrear,usuario: dict = Depends(obtener_usuario_actual), db: Session = Depends(get_db)):
     producto_db = db.query(Producto).filter(Producto.codigo == nuevo_detalle.codigo).first() #existe producto
     if producto_db is None:
         raise HTTPException(status_code=404, detail="Producto no encontrado")
@@ -73,7 +74,7 @@ def agregar_detalle(id_venta: int, nuevo_detalle : DetalleCrear, db: Session = D
     return {"mensaje": mensaje, "alertas": alertas}
 
 @router.delete("/ventas/borrar/detalle/{detalle_id}")
-def borrar_detalle(detalle_id: int, db: Session = Depends(get_db) ):
+def borrar_detalle(detalle_id: int, usuario: dict = Depends(obtener_usuario_actual),db: Session = Depends(get_db) ):
     detalle_db = db.query(DetalleVenta).filter(DetalleVenta.id == detalle_id).first()
     if detalle_db is None:
         raise HTTPException(status_code=404, detail="Detalle no encontrado")
@@ -87,7 +88,7 @@ def borrar_detalle(detalle_id: int, db: Session = Depends(get_db) ):
     return {"mensaje":"Detalle eliminado!" }
 
 @router.put("/ventas/editar/detalle/{detalle_id}")
-def editar_detalle(detalle_id: int, cantidad_nueva : int, db: Session = Depends(get_db)):
+def editar_detalle(detalle_id: int, cantidad_nueva : int, usuario: dict = Depends(obtener_usuario_actual),db: Session = Depends(get_db)):
     detalle_db = db.query(DetalleVenta).filter(DetalleVenta.id == detalle_id).first()
     if detalle_db is None:
         raise HTTPException(status_code=404, detail="Detalle no encontrado")
@@ -98,8 +99,11 @@ def editar_detalle(detalle_id: int, cantidad_nueva : int, db: Session = Depends(
     db.commit()
     return detalle_db
 
+# Solo admin
 @router.get("/ventas/reporte/acumulacion/{fecha_inicio}/{fecha_fin}")
-def reporte_por_periodo_ventas(fecha_inicio: date, fecha_fin: date, db: Session = Depends(get_db)):
+def reporte_por_periodo_ventas(fecha_inicio: date, fecha_fin: date,usuario: dict = Depends(obtener_usuario_actual), db: Session = Depends(get_db)):
+    if usuario["rol"] != "admin":
+        raise HTTPException(status_code=403, detail="No tenés permiso")
     ventas_db = db.query(Venta).filter(Venta.fecha >= fecha_inicio, Venta.fecha <= fecha_fin).all() # siempre devuelve una lista iterable
     total_periodo = 0
     if not ventas_db:
@@ -108,8 +112,10 @@ def reporte_por_periodo_ventas(fecha_inicio: date, fecha_fin: date, db: Session 
         total_periodo += venta.total
     return total_periodo
 
-@router.get("/ventas/reporte/productos/{fecha_inicio}/{fecha_fin}")
-def reporte_productos_mas_vendidos(fecha_inicio: date, fecha_fin: date,db: Session = Depends(get_db)):
+@router.get("/ventas/reporte/productos/{fecha_inicio}/{fecha_fin}") #admin elija cuantos quiere ver
+def reporte_productos_mas_vendidos(fecha_inicio: date, fecha_fin: date,usuario: dict = Depends(obtener_usuario_actual),db: Session = Depends(get_db)):
+    if usuario["rol"] != "admin":
+        raise HTTPException(status_code=403, detail="No tenés permiso")
     ventas_db = db.query(Venta).filter(func.date(Venta.fecha) >= fecha_inicio, func.date(Venta.fecha) <= fecha_fin).all()
     dicc_producto_cantidad = {}
     for venta in ventas_db:
