a4d9cb4c3a12ff5d2af727bf34710121446bc06d
SolBFigueroa <figueroa.solb@gmail.com>
2026-06-10 19:31:47 -0300

    agrego CORS para permitir peticiones del frontend

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.database import engine, Base
 from app.models import usuarios, productos, ventas
 from app.routes import productos, usuarios, ventas
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5500", "http://127.0.0.1:5500"], # backend acepta peticiones de esas urls
+    allow_methods=["*"], # * = todos
+    allow_headers=["*"],
+)
 # acá van a ir los routers después
 app.include_router(usuarios.router)
 app.include_router(productos.router)
